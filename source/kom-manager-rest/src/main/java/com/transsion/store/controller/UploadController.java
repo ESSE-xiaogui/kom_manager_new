b9548bdbedc3d00647f1c24bf473a27e2930d54b
liuzh <zhonghua.liu@transsion.com>
2016-12-30 16:08:22 +0800

    实现文件上传功能

--- source/kom-manager-rest/src/main/java/com/transsion/store/controller/UploadController.java
+++ source/kom-manager-rest/src/main/java/com/transsion/store/controller/UploadController.java
@@ -1,45 +1,38 @@
 package com.transsion.store.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
-import java.util.Map;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-
+import com.rest.service.codec.response.StandardResult;
+import net.mikesu.fastdfs.FastdfsClient;
+import net.mikesu.fastdfs.FastdfsClientFactory;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.springframework.stereotype.Controller;
-
-import com.rest.service.codec.response.StandardResult;
-
-import net.mikesu.fastdfs.FastdfsClient;
-import net.mikesu.fastdfs.FastdfsClientFactory;
 import sun.misc.BASE64Encoder;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.*;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @Path("/fastdfs")
 public class UploadController {
 	private static FastdfsClient fastdfsClient = FastdfsClientFactory.getFastdfsClient();
+    private static final String configFile = "FastdfsClient.properties";
 	private static final String SERVER_UPLOAD_LOCATION_FOLDER = "";
+
     @POST
     @Path("/upload")
     @Consumes({MediaType.MULTIPART_FORM_DATA})
     @Produces({MediaType.APPLICATION_JSON})
-    public StandardResult uploadFile(MultipartFormDataInput input) {
+    public Object uploadFile(MultipartFormDataInput input) {
         String fileName = "";
         Map<String, List<InputPart>> formParts = input.getFormDataMap();
-        List<InputPart> inPart = formParts.get("file");
+        List<InputPart> inPart = formParts.get("files");
         for (InputPart inputPart : inPart) {
 
             try {
@@ -52,8 +45,17 @@ public class UploadController {
 
                 fileName = SERVER_UPLOAD_LOCATION_FOLDER + fileName;
                 BASE64Encoder encode = new BASE64Encoder();
-                String fileId = uploadFile(istream,fileName);
-                return new StandardResult(new String[]{fileId,fileName,encode.encode(fileId.getBytes())});
+                Files files = new Files();
+
+                String fileId = uploadFile(istream,fileName, files);
+
+                files.setFileId(fileId);
+                files.setName(fileId);
+                files.setUrl(PropertiesConfigurationHelper.getInstance().getUploadServer() + fileId);
+                files.setThumbnailUrl(files.getUrl());
+                files.setFileIdEncode(encode.encode(fileId.getBytes()));
+
+                return new FileResult(files);
             }catch (IOException e) {
                 e.printStackTrace();
                 return new StandardResult(500,e.getMessage());
@@ -82,7 +84,7 @@ public class UploadController {
         }
         return "randomName";
     }
-	private static void saveFile(InputStream uploadedInputStream, File serverLocation) {
+	private static void saveFile(InputStream uploadedInputStream, File serverLocation, Files files) {
 
 		try {
 			OutputStream outpuStream = null;
@@ -93,6 +95,7 @@ public class UploadController {
 			while ((read = uploadedInputStream.read(bytes)) != -1) {
 				outpuStream.write(bytes, 0, read);
 			}
+			files.setSize(serverLocation.length());
 			outpuStream.flush();
 			outpuStream.close();
 		} catch (IOException e) {
@@ -101,35 +104,132 @@ public class UploadController {
 		}
 	}
 
-	private static String uploadFile(InputStream uploadedInputStream, String serverLocation) throws Exception {
+	private static String uploadFile(InputStream uploadedInputStream, String serverLocation, Files files) throws Exception {
 		File uploadFile = new File(serverLocation);
-		saveFile(uploadedInputStream, uploadFile);
+		saveFile(uploadedInputStream, uploadFile, files);
 		String fileId = fastdfsClient.upload(uploadFile);
 		uploadFile.delete();
 		return fileId;
 	}
+
+	private final class FileResult {
+        private Files[] files;
+
+        public FileResult(Files... files) {
+            this.files = files;
+        }
+
+        public Files[] getFiles() {
+            return files;
+        }
+
+        public void setFiles(Files... files) {
+            this.files = files;
+        }
+    }
+	private final class Files {
+        private String fileId;
+        private String url;
+        private String name;
+        private String thumbnailUrl;
+        private long size;
+        private String fileIdEncode;
+
+        public String getFileId() {
+            return fileId;
+        }
+
+        public void setFileId(String fileId) {
+            this.fileId = fileId;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public long getSize() {
+            return size;
+        }
+
+        public void setSize(long size) {
+            this.size = size;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getThumbnailUrl() {
+            return thumbnailUrl;
+        }
+
+        public void setThumbnailUrl(String thumbnailUrl) {
+            this.thumbnailUrl = thumbnailUrl;
+        }
+
+        public String getFileIdEncode() {
+            return fileIdEncode;
+        }
+
+        public void setFileIdEncode(String fileIdEncode) {
+            this.fileIdEncode = fileIdEncode;
+        }
+    }
 	
+    private final static class PropertiesConfigurationHelper {
+        private String uploadServer;
 
+        private static PropertiesConfigurationHelper getInstance(){
+            return new PropertiesConfigurationHelper();
+        }
+        private PropertiesConfigurationHelper(){
+            try {
+                PropertiesConfiguration config = new PropertiesConfiguration(configFile);
+
+                this.uploadServer = config.getString("upload_server");
+            } catch (ConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String getUploadServer() {
+            return uploadServer;
+        }
+
+        public void setUploadServer(String uploadServer) {
+            this.uploadServer = uploadServer;
+        }
+    }
+
+    class BigFileOutputStream implements javax.ws.rs.core.StreamingOutput {
+        private InputStream inputStream;
+        public BigFileOutputStream(){}
+        public BigFileOutputStream(InputStream inputStream)
+        {
+            this.inputStream = inputStream;
+        }
+
+        @Override
+        public void write(OutputStream output) throws IOException,
+                WebApplicationException {
+            // TODO Auto-generated method stub
+            IOUtils.copy(inputStream, output);
+        }
+
+        public InputStream getInputStream() {
+            return inputStream;
+        }
+        public void setInputStream(InputStream inputStream) {
+            this.inputStream = inputStream;
+        }
+    }
 }
-class BigFileOutputStream implements javax.ws.rs.core.StreamingOutput {
-    private InputStream inputStream;
-    public BigFileOutputStream(){}
-    public BigFileOutputStream(InputStream inputStream)
-    {
-        this.inputStream = inputStream;
-    }
 
-    @Override
-    public void write(OutputStream output) throws IOException,
-            WebApplicationException {
-        // TODO Auto-generated method stub
-        IOUtils.copy(inputStream, output);
-    }
-
-    public InputStream getInputStream() {
-        return inputStream;
-    }
-    public void setInputStream(InputStream inputStream) {
-        this.inputStream = inputStream;
-    }
-}
