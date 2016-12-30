6036f64fa85d67a12a1966ef66b5202844c6b16b
ghzhang <guihua.zhang@transsion.com>
2016-12-30 11:10:47 +0800

    异常信息抛出修改

--- source/kom-manager-api/src/main/java/com/transsion/store/resource/ExceptionDef.java
+++ source/kom-manager-api/src/main/java/com/transsion/store/resource/ExceptionDef.java
@@ -3,8 +3,22 @@ package com.transsion.store.resource;
 public enum ExceptionDef {
 
 	USER_ERROR(100, "USER_ERROR"),
-	USER_ERROR_LOGIN(101, "USER_ERROR_LOGIN");
+	USER_ERROR_LOGIN(101, "USER_ERROR_LOGIN"),
 
+	SALE_ERROR(200, "SALE_ERROR"),
+	
+	STOCK_ERROR(300, "STOCK_ERROR"),
+	
+	BASIC_ERROR(400, "BASIC_ERROR"),
+	
+	BRAND_ERROR(500, "BRAND_ERROR"),
+	
+	MODEL_ERROR(600, "MODEL_ERROR"),
+	
+	SHOP_ERROR(700, "SHOP_ERROR"),
+	
+	REGION_ERROR(800, "REGION_ERROR");
+	
 	private int value;
 	private String desc;
 
