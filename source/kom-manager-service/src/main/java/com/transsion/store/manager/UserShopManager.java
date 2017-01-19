7259635a7d721d9f322601b1b9504fe24c3a20f7
ghzhang <guihua.zhang@transsion.com>
2017-01-19 13:35:49 +0800

    用户绑定店铺代码修改

--- source/kom-manager-service/src/main/java/com/transsion/store/manager/UserShopManager.java
+++ source/kom-manager-service/src/main/java/com/transsion/store/manager/UserShopManager.java
@@ -1,6 +1,4 @@
 package com.transsion.store.manager;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.shangkang.core.exception.ServiceException;
@@ -9,32 +7,32 @@ import com.transsion.store.bo.UserShop;
 import com.transsion.store.dto.UserShopDto;
 import com.transsion.store.mapper.UserShopMapper;
 import com.transsion.store.resource.MessageStoreResource;
+import com.transsion.store.service.SystemDateService;
 @Service("userShopManager")
 public class UserShopManager {
 	
 	@Autowired
 	private UserShopMapper userShopMapper;
+	@Autowired
+	private SystemDateService systemDateService;
 	
 	/**
 	 * 门店授权管理:用户绑定门店
 	 * */
 	public void saveUserShop(UserShopDto userShopDto) throws ServiceException{
-		if(UtilHelper.isEmpty(userShopDto) || UtilHelper.isEmpty(userShopDto.getUserId()) 
-						|| UtilHelper.isEmpty(userShopDto.getShopId())){
+		if(UtilHelper.isEmpty(userShopDto) && UtilHelper.isEmpty(userShopDto.getUserId())){
 			throw new ServiceException(MessageStoreResource.ERROR_MESSAGE_PARAM_IS_NULL);
 		}
 		userShopMapper.deleteByUserId(userShopDto.getUserId());
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
-		String date = sdf.format(new Date());
-		for(Long shopId:userShopDto.getShopId()){
-			
+		if(!UtilHelper.isEmpty(userShopDto.getShopId())){
+			for(Long shopId:userShopDto.getShopId()){
 			UserShop userShop = new UserShop();
 			userShop.setUserId(userShopDto.getUserId());
 			userShop.setShopId(shopId);
 			userShop.setCreateBy(userShopDto.getUserId().toString());
-			userShop.setCreatedTime(date);
+			userShop.setCreatedTime(systemDateService.getCurrentDate());
 			userShopMapper.saveUserShop(userShop);
 		}
-		
+		}
 	}
 }
