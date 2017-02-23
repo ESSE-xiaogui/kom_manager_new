9097750a1c06fee7674d6fb6be7b78ef2b0f6a08
dongdong.xue <dongdong.xue@RLK-F0747-xdd.reallytek.com>
2017-02-23 11:22:24 +0800

    属性查询

--- source/kom-manager-rest/src/main/java/com/transsion/store/controller/AttributeController.java
+++ source/kom-manager-rest/src/main/java/com/transsion/store/controller/AttributeController.java
@@ -3,6 +3,7 @@ package com.transsion.store.controller;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -27,7 +28,7 @@ public class AttributeController extends AbstractController{
 	 * @param type
 	 * @return
 	 */
-	@GET
+	@POST
 	@Path("/getAttributeListByType")
 	@Produces({MediaType.APPLICATION_JSON})
 	public List<Attribute> getAttributeListByType(Type type)throws ServiceException{
