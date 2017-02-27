713abaced7bf1403556b2d0ceda35c6608a2d511
hujianmin <jianmin.hu@transsion.com>
2017-02-27 11:34:37 +0800

    kom-manager:add VisitHistorySummary file

---
+++ source/kom-manager-api/src/main/java/com/transsion/store/dto/VisitHistorySummaryDto.java
@@ -0,0 +1,34 @@
+package com.transsion.store.dto;
+
+import java.io.Serializable;
+
+public class VisitHistorySummaryDto  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String visitDate;
+	
+	private int visitStoreQty;
+
+	public String getVisitDate() {
+		return visitDate;
+	}
+
+	public void setVisitDate(String visitDate) {
+		this.visitDate = visitDate;
+	}
+
+	public int getVisitStoreQty() {
+		return visitStoreQty;
+	}
+
+	public void setVisitStoreQty(int visitStoreQty) {
+		this.visitStoreQty = visitStoreQty;
+	}
+
+	@Override
+	public String toString() {
+		return "VisitHistorySummaryDto [visitDate=" + visitDate + ", visitStoreQty=" + visitStoreQty + "]";
+	}
+	
+}
