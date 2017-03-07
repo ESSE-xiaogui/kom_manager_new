3f38dbb0aebf70f4439663edd096e71844564349
yang.zhang2 <yang.zhang2@A4008834-ZY.reallytek.com>
2017-03-07 19:58:52 +0800

    销售日报代码更新

--- source/kom-manager-api/src/main/java/com/transsion/store/dto/ReportSaleDailyDto.java
+++ source/kom-manager-api/src/main/java/com/transsion/store/dto/ReportSaleDailyDto.java
@@ -13,8 +13,12 @@ public class ReportSaleDailyDto extends ReportSaleDaily implements Serializable 
 	
 	private Long grandId;				// 门面等级ID
 	private String gradeName;			// 门店等级
+	private Long brandId;				// 品牌ID
+	private Long modelId;				// 机型ID
 	private String beginSaleTime;		// 开始日期
 	private String endSaleTime;			// 结束日期
+	private Long regionId;				// 区域ID
+	private String regionType;			// 区域类型 2:国家 3： 4：城市
 	
 	public Long getGrandId() {
 		return grandId;
@@ -30,6 +34,18 @@ public class ReportSaleDailyDto extends ReportSaleDaily implements Serializable 
 		this.gradeName = gradeName;
 	}
 	
+	public Long getBrandId() {
+		return brandId;
+	}
+	public void setBrandId(Long brandId) {
+		this.brandId = brandId;
+	}
+	public Long getModelId() {
+		return modelId;
+	}
+	public void setModelId(Long modelId) {
+		this.modelId = modelId;
+	}
 	public String getBeginSaleTime() {
 		return beginSaleTime;
 	}
@@ -42,5 +58,19 @@ public class ReportSaleDailyDto extends ReportSaleDaily implements Serializable 
 	}
 	public void setEndSaleTime(String endSaleTime) {
 		this.endSaleTime = endSaleTime;
+	}
+	
+	public Long getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(Long regionId) {
+		this.regionId = regionId;
+	}
+	
+	public String getRegionType() {
+		return regionType;
+	}
+	public void setRegionType(String regionType) {
+		this.regionType = regionType;
 	}
 }
